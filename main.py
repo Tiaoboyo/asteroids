fc5207c4ad4fd1ac491816a0a15009da6555aee3
Sebastian Zusi <sebastianzusi@gmail.com>
2025-04-19 20:57:33 +0900

    Pausing for the night

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    fps = 60
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen, (0,0,0))
-        
         for event in pygame.event.get():
            if event.type == pygame.QUIT:
               return
-
+        dt = clock.tick(fps) / 100
+        print("DT is", dt)
         pygame.display.flip()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
